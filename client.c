fbb59eb98d7a07bfe2c102918188f65b7623d329
Zealth121 <C.riesmeyer7@outlook.com>
2023-08-16 14:29:36 -0400

    edited client and server. still want to figure out how to make multiple connections work

--- client.c
+++ client.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+
+#define CONNECTION_PORT 3500
+#define IP_ADDR "192.168.4.43"
 
 int main(){
 
@@ -17,20 +23,21 @@ int main(){
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     
     if(socket_desc < 0){
-        printf("Unable to create socket\n");
-        return -1;
+        perror("Unable to create socket\n");
+        exit(EXIT_FAILURE);
     }
     printf("Socket created successfully\n");
     
     // Set port and IP the same as server-side:
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(CONNECTION_PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_zero[8] = '\0';
     
     // Send connection request to server:
     if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
-        printf("Unable to connect\n");
-        return -1;
+        perror("Unable to connect\n");
+        exit(EXIT_FAILURE);
     }
     printf("Connected with server successfully\n");
     
@@ -40,14 +47,14 @@ int main(){
     
     // Send the message to server:
     if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
-        printf("Unable to send message\n");
-        return -1;
+        perror("Unable to send message\n");
+        exit(EXIT_FAILURE);
     }
     
     // Receive the server's response:
     if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
-        printf("Error while receiving server's msg\n");
-        return -1;
+        perror("Error while receiving server's msg\n");
+        exit(EXIT_FAILURE);
     }
     
     printf("Server's response: %s\n",server_message);
--- server.c
+++ server.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+
+#define CONNECTION_PORT 3500
+#define IP_ADDR "192.168.4.43"
 
 int main(){
 
     int socket_desc, client_sock, client_size;
     struct sockaddr_in server_addr, client_addr;
     char server_message[2000], client_message[2000];
+    int option_value = 1;
 
     //Clean buffers
     memset(server_message, '\0', sizeof(server_message));
@@ -17,27 +24,34 @@ int main(){
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
     if(socket_desc < 0){
-        printf("Error while creating socket\n");
-        return -1;
+        perror("Error while creating socket\n");
+        exit(EXIT_FAILURE);
     }
     printf("Socket created successfully\n");
 
+    if(setsockopt(socket_desc, SOL_SOCKET,SO_REUSEADDR , &option_value,sizeof(option_value)) < 0){
+        perror("Couldn't set options\n");
+        exit(EXIT_FAILURE);
+    }
+
     //set port and ip
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(CONNECTION_PORT);
+    //inet_addr(IP_ADDR)
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_zero[8] = '\0';
 
     // Bind to the port and ip
     if(bind(socket_desc, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0){
-        printf("Error binding server\n");
-        return -1;
+        perror("Error binding server\n");
+        exit(EXIT_FAILURE);
     }
     printf("Done with binding\n");
 
     //listen for clients
-    if(listen(socket_desc, 1) < 0){
-        printf("Error while listening\n");
-        return -1;
+    if(listen(socket_desc, 4) < 0){
+        perror("Error while listening\n");
+        exit(EXIT_FAILURE);
     }
     printf("Listening for incoming connections...\n");
 
@@ -46,15 +60,15 @@ int main(){
     client_sock = accept(socket_desc, (struct sockaddr*) &client_addr, &client_size);
 
     if(client_sock < 0){
-        printf("Can't accept\n");
-        return -1;
+        perror("Can't accept\n");
+        exit(EXIT_FAILURE);
     }
     printf("Client connected at IP: %i and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
     //Recieve clients message
     if(recv(client_sock, client_message, sizeof(client_message), 0) < 0){
-        printf("Couldn't recieve\n");
-        return -1;
+        perror("Couldn't recieve\n");
+        exit(EXIT_FAILURE);
     }
     printf("Msg from client: %s\n", client_message);
 
